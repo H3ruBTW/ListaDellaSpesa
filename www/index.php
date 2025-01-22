a3527d2604a5d96f4d99539471f5b03281998a89
H3ru BTW <alessandro.buongallino@gmail.com>
2025-01-22 15:10:58 +0000

    commit

--- www/index.php
+++ www/index.php
@@ -13,6 +13,16 @@
             </center>
         </div>
         <div id="content">
+            <center>
+                <p>
+                INSERIRE IL CODICE A BARRE<br>
+                QUI SOTTO
+                </p>
+                <form>
+                    <input type="text" name="barcode" placeholder="ES.: 2387456723563">
+                    <input type="submit">
+                </form>
+            </center>
         </div>
     </body>
 </html>
